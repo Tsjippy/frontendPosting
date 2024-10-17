8b09d1cce5bcdf0391eab4e2b200467afe613231
Tsjippy <ewaldha@hotmail.com>
2024-10-17 09:54:30 +0100

    changed: readme

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -19,18 +19,6 @@ add_filter('sim_submenu_description', function($description, $moduleSlug){
 
 	ob_start();
 
-	?>
-	<p>
-		This module makes it possible to add and edit pages, posts and custom post types.<br>
-		Just place this shortcode on any page: <code>[front_end_post]</code>.<br>
-		An overview of the posts created by the current user can be displayed using the: <code>[your_posts]</code> shortcode.<br>
-		If anyone without publish rights tries to add or edit a page, it will be stored as pending.<br>
-		An overview of pending content can be shown using the <code>[pending_pages]</code> shortcode.<br>
-		You can use the <code>[pending_post_icon]</code> shortcode as an indicator, displaying the amount of pending posts in menu items.<br>
-		This module also adds a custom post status: archived. Meaning a post is not visible but still kept for reference
-	</p>
-
-	<?php
 	$url		= SIM\ADMIN\getDefaultPageLink($moduleSlug, 'front_end_post_pages');
 	$url2		= SIM\ADMIN\getDefaultPageLink($moduleSlug, 'pending_pages');
 	
@@ -44,7 +32,7 @@ add_filter('sim_submenu_description', function($description, $moduleSlug){
 		<?php
 	}
 
-	return ob_get_clean();
+	return $description.ob_get_clean();
 }, 10, 2);
 
 add_filter('sim_submenu_options', function($optionsHtml, $moduleSlug, $settings){
